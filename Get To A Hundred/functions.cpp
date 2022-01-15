18f6b10d7532c57b9e6c96c323ee0bcd4ebf1c81
Michael <76692293+michaelrw19@users.noreply.github.com>
2022-01-14 23:38:35 -0500

    Update functions.cpp

--- Get To A Hundred/functions.cpp
+++ Get To A Hundred/functions.cpp
@@ -6,15 +6,14 @@
 using namespace std;
 
 //Return 1 if the input is proper, 0 otherwise
-int inputCheck(string in, char code) {
+int inputCheck(string in) {
 	if (in.compare("q") == 0) return -1;
 	else if (in.length() != 1 && in.length() != 2) return 0;
 	for (int i = 0; i < in.length(); i++) {
 		if (!isdigit(in[i])) return 0;
 	}
 	int n = stoi(in);
-	if (code == 'd' && (n < 1 || n > 3)) return 0;
-	if (code == 'i' && (n < 1 || n > 10)) return 0;
+	if (n < 1 || n > 10) return 0;
 	return n;
 }
 
@@ -34,7 +33,7 @@ void printText(string diff, int randnum) {
 		<< "\n2. Enter a number between 1-10 that will be added to the starting number"
 		<< "\n3. We will enter our own number one at a time"
 		<< "\n4. Player who reaches to 100 first wins"
-		<< "\n4. Player who exceeds beyond 100 loses"
+		<< "\n5. Player who exceeds beyond 100 loses"
 		<< "\n========================================================================" << endl;
 	cout << "Level of difficulty: " << diff << endl;
 	cout << "\nA random number has been rolled. If that number is 1 you go first, else the computer goes first."
@@ -43,16 +42,16 @@ void printText(string diff, int randnum) {
 
 //Return 1 if the player want to play again, 0 otherwise
 int playAgain() {
-	string in = "";
-	while (in.compare("y") != 0 || in.compare("n") != 0 || in.compare("q") != 0) {
+	string userInput = "";
+	while (userInput.compare("y") != 0 || userInput.compare("n") != 0 || userInput.compare("q") != 0) {
 		cout << "Play again? (y/n): ";
-		getline(cin, in);
-		in.erase(remove(in.begin(), in.end(), ' '), in.end()); //remove every occurance of space
-		in.erase(remove(in.begin(), in.end(), '\t'), in.end()); //remove every occurance of tab
+		getline(cin, userInput);
+		userInput.erase(remove(userInput.begin(), userInput.end(), ' '), userInput.end()); //remove every occurance of space
+		userInput.erase(remove(userInput.begin(), userInput.end(), '\t'), userInput.end()); //remove every occurance of tab
 
-		if (in.length() == 1) {
-			if (tolower(in[0]) == 'y') return 1;
-			else if (tolower(in[0]) == 'n' || tolower(in[0]) == 'q') exit(0);
+		if (userInput.length() == 1) {
+			if (tolower(userInput[0]) == 'y') return 1;
+			else if (tolower(userInput[0]) == 'n' || tolower(userInput[0]) == 'q') return 0;
 		}
 		system("CLS");
 	}
