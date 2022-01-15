c897242660d008fb8fa9a0c7348a9208c42b6058
Michael <76692293+michaelrw19@users.noreply.github.com>
2022-01-14 23:41:27 -0500

    Update functions.h

--- Get To A Hundred/functions.h
+++ Get To A Hundred/functions.h
@@ -6,11 +6,7 @@ int inputCheck(string in);
 
 int getWinningNum(int tTemp, int* winningNums);
 
-void printText(string diff, int randnum);
-
-//void printRecord(List* total, List* computer, List* player, int randnum);
-
-//void deleteClasses(List* total, List* computer, List* player, Difficulty* diff);
+void printText(int mode, string diff, int randnum);
 
 int playAgain();
 
